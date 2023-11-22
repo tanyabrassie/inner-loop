441095e03fcc5f0884941b76476249e9561ab1aa
Tanya Brassie <tanya.brassie@gmail.com>
2023-11-21 23:20:12 -0500

    updating dev dependency

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,7 +6,7 @@ import { NewsContent } from "./NewsContent";
 import { FairContent } from "./FairContent";
 import { LoadScreen } from "./LoadScreen";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { ServicesContent } from "./ServicesContent";
+// import { ServicesContent } from "./ServicesContent";
 
 const ContentSection = styled.main`
   padding: 40px;
